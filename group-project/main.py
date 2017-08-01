e5d965bb7c4aa77c48504eed8ef14ec89ce6de0e
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 10:16:04 -0500

    messgae

--- group-project/main.py
+++ group-project/main.py
@@ -20,6 +20,11 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class ProfileHandler(webapp2.RequestHandler):
+        def get(self):
+            
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
