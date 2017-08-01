cbffffd87fd708df37c2ec2039b5dcbdc4b56245
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 10:48:48 -0500

    The new nav bar

--- hero.js
+++ hero.js
@@ -1,6 +1,7 @@
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
 }
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
