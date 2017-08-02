541a755d4003ca4fb5593457010d7891b38f271f
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 16:35:10 -0500

    changes to main.py

--- group-project/main.py
+++ group-project/main.py
@@ -22,6 +22,8 @@ import urllib
 import urllib2
 import json
 from models import BlogModel
+from models import UserModel
+from google.appengine.api import users
 
 
 #set up environment for Jinja
@@ -69,6 +71,19 @@ class BlogshowHandler(webapp2.RequestHandler):
             template = jinja_environment.get_template("templates/blogshow.html")
             render_data = {}
             self.response.write(template.render(render_data))
+
+class UsersHandler(webapp2.RequestHandler):
+        def get(self):
+             user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                users.create_login_url('/'))
+
+        self.response.write('<html><body>%s</body></html>' % greeting
+
 
 
 app = webapp2.WSGIApplication([
