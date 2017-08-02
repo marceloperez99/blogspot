cc06902025fba4eb5ad19213710f2b37f9d75f3b
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 17:20:49 -0500

    added userapi

--- group-project/main.py
+++ group-project/main.py
@@ -85,7 +85,16 @@ class BlogshowHandler(webapp2.RequestHandler):
 #
 #         self.response.write('<html><body>%s</body></html>' % greeting)
 
-
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                users.create_login_url('/prof'))
+        self.response.write('<html><body>%s</body></html>' % greeting)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
@@ -93,6 +102,6 @@ app = webapp2.WSGIApplication([
     ('/add', BlogaddHandler),
     ('/prof', ProfileHandler),
     ('/contact', ContactHandler),
-    # ('/login', UsersHandler),
+    ('/login', LoginHandler),
 
 ], debug=True)
